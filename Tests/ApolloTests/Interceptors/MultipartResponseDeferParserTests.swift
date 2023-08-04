2ecb5e9f3cb5f46182a4c47970231c485025f282
Calvin Cestari <calvin.cestari@apollographql.com>
2023-08-04 10:29:45 -0700

    Rename and add new test file

---
+++ Tests/ApolloTests/Interceptors/MultipartResponseDeferParserTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+import Nimble
+@testable import Apollo
+import ApolloAPI
+import ApolloInternalTestHelpers
+
+final class MultipartResponseDeferParserTests: XCTestCase {
+
+  let defaultTimeout = 0.5
+
+  // MARK: - Error tests
+
+  // MARK: Parsing tests
+
+}
